e43a19d305ab9b1f94a2e9ab569659e08e6400bb
JingAn Chen <lzsoft.cja@gmail.com>
2020-03-02 23:16:28 +0800

    Portfolio panel is done

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ window.addEventListener("load", function () {
 window.particlesJS.load('particles', 'assets/particles.json', function () {
 	console.log('callback - particles.js config loaded');
 });
+document.querySelector("body > main#portfolio > nav.right").addEventListener("click", function () {
+	document.querySelector("body > main#portfolio > main").scrollBy({
+		top: 0,
+		left: window.innerWidth,
+		behavior: "smooth"
+	});
+});
+document.querySelector("body > main#portfolio > nav.left").addEventListener("click", function () {
+	document.querySelector("body > main#portfolio > main").scrollBy({
+		top: 0,
+		left: 0 - window.innerWidth,
+		behavior: "smooth"
+	});
+});
