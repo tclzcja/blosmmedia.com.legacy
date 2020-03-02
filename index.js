4eb6e41132ef71a3b40fe82fba504582d64994e7
JingAn Chen <lzsoft.cja@gmail.com>
2020-03-03 00:15:55 +0800

    Reform the assets strcture

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function () {
 	document.body.classList.remove("off");
 })
-window.particlesJS.load('particles', 'assets/particles.json', function () {
+window.particlesJS.load('particles', 'config/particles.json', function () {
 	console.log('callback - particles.js config loaded');
 });
 document.querySelector("body > main#portfolio > nav.right").addEventListener("click", function () {
