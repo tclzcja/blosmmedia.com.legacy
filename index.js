4e4045cc4502547ab5ddbb0f0299b23bc230455b
JingAn Chen <lzsoft.cja@gmail.com>
2020-03-07 14:47:45 +0800

    Minor fix

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ window.addEventListener("load", function () {
 window.particlesJS.load('particles', 'config/particles.json', function () {
 	console.log('callback - particles.js config loaded');
 });
-document.querySelector("body > header > header > select").value = navigator.language;
-document.querySelector("body > header > header > select").addEventListener("change", function () {
-	window.dispatchEvent(new CustomEvent("LANGUAGE", {
-		detail: {
-			"lang": this.value
-		}
-	}));
-});
 document.querySelector("body > main#portfolio > nav.right").addEventListener("click", function () {
 	document.querySelector("body > main#portfolio > main").scrollBy({
 		top: 0,
@@ -37,3 +29,12 @@ document.querySelectorAll("body > main#portfolio > main > section > a").addEvent
 document.querySelectorAll("body > main#portfolio > main > section > a").addEventListener("mouseleave", function () {
 	this.parentNode.parentNode.parentNode.classList.remove("on");
 })
+document.querySelector("body > header > header > select").value = navigator.language;
+document.querySelector("body > header > header > select").addEventListener("change", function () {
+	window.dispatchEvent(new CustomEvent("LANGUAGE", {
+		detail: {
+			"lang": this.value
+		}
+	}));
+});
+document.querySelector("body > header > header > select").dispatchEvent(new Event("change"));
